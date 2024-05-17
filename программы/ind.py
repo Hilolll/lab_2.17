442504b4023c8b1db2bcade735d32c09178dd2f8
Hiloll <hilollken@gmail.com>
2024-05-17 12:24:49 +0300

    код

---
+++ программы/ind.py
@@ -0,0 +1,136 @@
+import json
+import sys
+
+
+def get_route():
+    """
+    Запросить данные о списке
+    """
+    start = input("Ведите фамилию, имя ")
+    finish = input("Введите знак Зодиака ")
+    zodiac = (input("Введите дату рождения "))
+
+    return {
+        'start': start,
+        'finish': finish,
+        'zodiac': zodiac,
+    }
+
+
+def display_route(routes):
+    """
+    Отобразить список
+    """
+    if routes:
+        line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+            '-' * 4,
+            '-' * 30,
+            '-' * 20,
+            '-' * 14
+        )
+        print(line)
+        print(
+            '| {:^4} | {:^30} | {:^20} | {:^14} |'.format(
+                "№",
+                "Фамилия, имя",
+                "Знак Зодиака",
+                "Дата рождения"
+            )
+        )
+        print(line)
+
+        for idx, worker in enumerate(routes, 1):
+            print(
+                '| {:>4} | {:<30} | {:<20} | {:>14} |'.format(
+                    idx,
+                    worker.get('start', ''),
+                    worker.get('finish', ''),
+                    worker.get('zodiac', 0)
+                )
+            )
+        print(line)
+    else:
+        print("Список пуст")
+
+
+def select_route(routes, period):
+    """
+    Выбрать зодиак
+    """
+    result = []
+    for employee in routes:
+        if employee.get('finish') == period:
+            result.append(employee)
+
+    return result
+
+
+def save_routes(file_name, staff):
+    """
+    Сохранить данные в файл JSON
+    """
+    with open(file_name, "w", encoding="utf-8") as fout:
+        json.dump(staff, fout, ensure_ascii=False, indent=4)
+
+
+def load_routes(file_name):
+    """
+    Загрузить данные из файла JSON
+    """
+    with open(file_name, "r", encoding="utf-8") as fin:
+        return json.load(fin)
+
+
+def main():
+    """
+    Главная функция программы
+    """
+    routes = []
+
+    while True:
+        command = input(">>> ").lower()
+        if command == 'exit':
+            break
+
+        elif command == 'add':
+            route = get_route()
+            routes.append(route)
+            routes.sort(key=lambda item: int(item.get('zodiac', '').split('.')[2]))
+
+        elif command == 'list':
+            display_route(routes)
+
+        elif command.startswith('select'):
+            parts = command.split(' ', maxsplit=1)
+            period = parts[1].strip()  # Получаем название знака Зодиака
+            selected = select_route(routes, period)
+            if selected:
+                display_route(selected)
+            else:
+                print("Нет людей с таким знаком Зодиака.")
+
+        elif command.startswith("save "):
+            parts = command.split(maxsplit=1)
+            file_name = parts[1]
+            save_routes(file_name, routes)
+
+        elif command.startswith("load "):
+            parts = command.split(maxsplit=1)
+            file_name = parts[1]
+            routes = load_routes(file_name)
+
+        elif command == 'help':
+            print("Список команд:\n")
+            print("add - добавить знак зодиака;")
+            print("list - вывести список;")
+            print("select <список знаков зодиака> - запросить данные о зодиаке;")
+            print("help - отобразить справку;")
+            print("load - загрузить данные из файла;")
+            print("save - сохранить данные в файл;")
+            print("exit - завершить работу с программой.")
+        else:
+            print(f"Неизвестная команда {command}", file=sys.stderr)
+
+
+if __name__ == '__main__':
+    main()
